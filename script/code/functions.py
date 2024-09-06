349afb9cbcd2bc809b4c1463cca7ce4af188474f
jpbezerra <jubalima2005@gmail.com>
2024-09-06 01:52:32 -0300

    Update

---
+++ script/code/functions.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from config import my_colors, hours_name, months_name
+
+# talvez retornar months e hours pra calcular medidas e realizar hipóteses
+def months_and_hours_review(data: np.ndarray, y: str, title: str, target_label='Date') -> None:
+    months = np.zeros(12, dtype=int)
+    hours = np.zeros(24, dtype=int)
+
+    for label in data:
+        date, time = label[target_label].split(' ')
+
+        months[int(date.split('-')[1]) - 1] += 1
+        hours[int(time.split(':')[0])] += 1
+
+    # barplot of date
+    plt.bar(
+        x=months_name,
+        height=months,
+        color=my_colors
+    )
+
+    plt.xlabel("Months")
+    plt.ylabel(f"Amount of {y}")
+    plt.title(f"Relação entre meses do ano e {title}")
+    plt.show()
+
+    # time
+    plt.bar(
+        x=hours_name,
+        height=hours,
+        color=my_colors
+    )
+
+    plt.xlabel("Hours")
+    plt.ylabel(f"Amount of {y}")
+    plt.title(f"Relação entre horas do dia e {title}")
+    plt.show()
+
+def pie_review(data : np.ndarray, target_label : str, title : str) -> None:
+    information : dict[int] = {}
+
+    for info in data:
+        target = info[target_label]
+
+        try:
+            information[target] += 1
+
+        except KeyError:
+            information[target] = 1
+
+    plt.pie(
+        x=information.values(), labels=information.keys(), autopct='%1.1f%%', colors=my_colors
+    )
+    plt.title(f"Quantidade de {title}")
+    plt.show()
+
+def unique(data : np.ndarray, target_label : str) -> np.ndarray:
+    result : np.ndarray = np.array([])
+
+    for info in data:
+        result = np.append(result, info[target_label])
+
+    return np.unique(result)
--- script/code/main.py
+++ script/code/main.py
@@ -1,7 +1,14 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-from config import*
+
+from script.config import *
+from script.functions import months_and_hours_review, pie_review, unique
+
+# TO-DO
+# ver se todos os gráficos estão de acordo com a proposta da análise ou se possui algum gráfico melhor para tal análise
+# fazer testes de hipótese
+# comentar o código
 
 class Data:
     data_json_path = data_path
@@ -12,9 +19,6 @@ class Data:
 
             file.close()
 
-        self.get_main_labels()
-
-    def get_main_labels(self) -> None:
         self.activity = self.data["Activity"]
         self.ads = self.data['Ads and data']
         self.settings = self.data['App Settings']
@@ -28,77 +32,85 @@ class Data:
         self.video = self.data['Video']
 
 def messages_analysis(data: Data) -> None:
-    # gráfico de pizza em relação à quantidade de mensages de cada usuário, colocar o nome de label com o username
     accounts = np.array([])
     total_messages = np.array([])
+
+    my_messages = np.array([])
+    other_messages = np.array([])
 
     for key, value in data.messages['Chat History']['ChatHistory'].items():
         accounts = np.append(accounts, key.split(' ')[-1].split(':')[0])
         total_messages = np.append(total_messages, len(value))
 
-    # gráfico bom, mudar cores e tentar fazer com que os nomes não se sobressaiam
-    # talvez mudar o tipo de gráfico
-    plt.style.use('_mpl-gallery-nogrid')
-    plt.pie(total_messages, labels=accounts, autopct='%1.1f%%')
+        for info in value:
+            if info['From'] == "jp__bezerra":
+                my_messages = np.append(my_messages, info)
+
+            else:
+                other_messages = np.append(other_messages, info)
+
+    plt.pie(
+            x=total_messages, labels=accounts, autopct='%1.1f%%', colors=my_colors,
+            pctdistance=0.85
+    )
+
     plt.title("Quantidade de mensagens recebidas por conta")
     plt.show()
 
+    months_and_hours_review(my_messages, "messages", "quantidade de mensagens enviadas por mim")
+    months_and_hours_review(other_messages, "messages", "quantidade de mensagens enviadas por outros usuários")
+
 def comments_analysis(data : Data) -> None:
-    months = np.zeros(12, dtype=int)
-    timetable = np.zeros(24, dtype=int)
+    comments = np.array(data.comments['Comments']['CommentsList'])
+    months_and_hours_review(comments, "comments", "quantidade de comentários")
 
-    for comment in data.comments['Comments']['CommentsList']:
-        date, time = comment['Date'].split(' ')
+    sizes = np.array([])
+    for comment in comments:
+        sizes = np.append(sizes, len(comment['Comment']))
 
-        months[int(date.split('-')[1]) - 1] += 1
-        timetable[int(time.split(':')[0])] += 1
+    print(f"Possuo a média de digitar {np.mean(sizes):.2f} caracteres por comentário feito")
 
-    # months graphic
-    plt.style.use('_mpl-gallery-nogrid')
+def activity_analysis(data : Data) -> None:
+    fav_video_list = np.array(data.activity['Favorite Videos']['FavoriteVideoList'])
+    months_and_hours_review(fav_video_list, "favorites", "quantidade de favoritos")
 
-    plt.bar(
-        x=[
-            "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ],
-        height=months,
-        color=my_colors
-    )
+    like_list = np.array(data.activity['Like List']['ItemFavoriteList'])
+    months_and_hours_review(like_list, "likes", "quantidade de curtidas")
+    # may - august
 
-    plt.xlabel("Months")
-    plt.ylabel("Amount of comments")
-    plt.title("Relação entre meses do ano e quantidade de comentários")
-    plt.show()
+    search_history = np.array(data.activity['Search History']['SearchList'])
+    months_and_hours_review(search_history, "searchs", "quantidade de pesquisas")
+    search_term = unique(search_history, 'SearchTerm')
+    print(f"I've searched {len(search_term)} different terms on tiktok")
+    # march - august
 
-    # timetable graphic
-    plt.style.use('_mpl-gallery')
+    share_history = np.array(data.activity['Share History']['ShareHistoryList'])
+    months_and_hours_review(share_history, "shares", "quantidade de compartilhamentos")
+    # march - august
 
-    plt.bar(
-        x=[
-            '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
-            '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
-            '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-            '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'
-        ],
-        height=timetable
-    )
+    video_browsing_history = np.array(data.activity['Video Browsing History']['VideoList'])
+    months_and_hours_review(video_browsing_history, "watched videos", "quantidade de vídeos assistidos")
+    # february - august
 
-    plt.xlabel("Hours")
-    plt.ylabel("Amount of comments")
-    plt.title("Relação entre horas do dia e quantidade de comentários")
-    plt.show()
+    login_history = np.array(data.activity['Login History']['LoginHistoryList'])
+    months_and_hours_review(login_history, "logins", "quantidade de logins")
+    # march - august
 
-    # podemos fazer uma hipótese qualquer aqui e ajeitar o gráfico, porque só é visivel se ajeitamos
-    # a config do matplotlib
-    # ver se utilizamos outro gráfico
+    pie_review(login_history, 'NetworkType', "logins por networks diferentes")
 
-def activity_review(data : Data):
-    pass
+def ads_analysis(data : Data) -> None:
+    ads_activity = np.array(data.ads['Off TikTok Activity']['OffTikTokActivityDataList'])
+    months_and_hours_review(ads_activity, "ads", "quantidade de anúncios", target_label='TimeStamp')
+    pie_review(ads_activity, 'Event', "diferentes reações à anúncios")
+    sources = unique(ads_activity, 'Source')
+    print(f"I've had {len(sources)} different source-ads on tiktok")
 
 def main() -> None:
     data = Data()
     messages_analysis(data)
     comments_analysis(data)
+    activity_analysis(data)
+    ads_analysis(data)
 
 if __name__ == "__main__":
     main()
